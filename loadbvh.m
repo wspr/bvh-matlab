8800e8a4f64ac69c40b2dc6bfcd77bb280bb6e13
Will Robertson <wspr81@gmail.com>
2012-06-22 15:07:37 +0930

    remove the example code from the loadbvh function!

--- loadbvh.m
+++ loadbvh.m
@@ -216,58 +216,12 @@ for nn = find([skeleton.parent] ~= 0)
 
 end
 
-%% Plot
 
-% Prepare the new video file.
-vidObj = VideoWriter('louise');
-open(vidObj);
 
-fincr = 5;
 
-ccaxis = [0 0 0 0 0 0];
-for ff = 500:fincr:1400
-  h = figure(1); clf; hold on
-  title(num2str(ff))
-  set(h,'color','white')
-  
-% From the BVH model exported by arena, it's clear that "y" is vertical
-% and "z" is medial-lateral. (From the "offsets" between the joints.)
-% Therefore, flip Y and Z when plotting to have Matlab's "vertical" z-axis
-% match up.
 
-  for nn = 1:Nnodes
-    
-    if false
-      if ~isempty(skeleton(nn).name)
-        text(skeleton(nn).Dxyz(1,ff),skeleton(nn).Dxyz(3,ff),skeleton(nn).Dxyz(2,ff),skeleton(nn).name)
-      end
-    end
-    plot3(skeleton(nn).Dxyz(1,ff),skeleton(nn).Dxyz(3,ff),skeleton(nn).Dxyz(2,ff),'.','markersize',20)
 
-    parent = skeleton(nn).parent;
-    if parent > 0
-      plot3([skeleton(parent).Dxyz(1,ff) skeleton(nn).Dxyz(1,ff)],...
-            [skeleton(parent).Dxyz(3,ff) skeleton(nn).Dxyz(3,ff)],...
-            [skeleton(parent).Dxyz(2,ff) skeleton(nn).Dxyz(2,ff)])
-    end
   end
-  
-  % plot c3D markers as well:
-  mrange = 1:Nmarkers;
-  mrange([4 35 36]) = [];
-  
-  for mm = mrange
-    plot3(-c3ddata(ff,mm,1)/10,c3ddata(ff,mm,2)/10,c3ddata(ff,mm,3)/10,'o','color','red')
-    % text(-c3ddata(ff,mm,1)/10,c3ddata(ff,mm,2)/10,c3ddata(ff,mm,3)/10,num2str(mm))
-  end
-  
-  view(-30,30)
-  axis([-100 200 0 200 -50 200])
-  
-  drawnow
-  
-  writeVideo(vidObj,getframe);
 
 end
 
-close(vidObj);
