22c120b9631308ac207dffe618f72d1661550c15
Will Robertson <wspr81@gmail.com>
2012-06-22 15:08:06 +0930

    tidy up/refactor slightly

--- loadbvh.m
+++ loadbvh.m
@@ -1,14 +1,9 @@
 function skeleton = loadbvh(name)
 %% LOADBVH  Load a .bvh file.
 
-name = 'louise';
 fname = [name,'.bvh'];
 
 %% Load and parse data
-
-% Markers:
-c3ddata = loadc3d([name,'.c3d']);
-Nmarkers = size(c3ddata,2);
 
 % File access:
 fid = fopen(fname);
@@ -27,24 +22,19 @@ while ~strcmp( C(ii) , 'MOTION' )
   
   ii = ii+1;
   
-  
   if strcmp( C(ii) , '{' )
-    
     brace_count = brace_count + 1;
-    
   elseif strcmp( C(ii) , '}' )
-    
     brace_count = brace_count - 1;
-    
   elseif strcmp( C(ii) , 'JOINT' ) || strcmp( C(ii) , 'ROOT' )
+    % Regular joint
     
     nn = nn+1;
     
     skeleton(nn).name = C(ii+1);
     skeleton(nn).offset = [str2double(C(ii+4)) ; str2double(C(ii+5)) ; str2double(C(ii+6))];
-    skeleton(nn).channels = str2double(C(ii+8));
+    skeleton(nn).Nchannels = str2double(C(ii+8));
     skeleton(nn).nestdepth = brace_count;
-    skeleton(nn).is_end_effector = false;
 
     if brace_count == 1
       % root node
@@ -57,22 +47,23 @@ while ~strcmp( C(ii) , 'MOTION' )
       skeleton(nn).parent = skeleton(brace_count).parent;
     end
     
-    if skeleton(nn).channels == 3
+    % What is the order of the rotations?
+    if skeleton(nn).Nchannels == 3
       skeleton(nn).order = [C{ii+9}(1),C{ii+10}(1),C{ii+11}(1)];
-    elseif skeleton(nn).channels == 6
+    elseif skeleton(nn).Nchannels == 6
       skeleton(nn).order = [C{ii+12}(1),C{ii+13}(1),C{ii+14}(1)];
     end
-    
-  elseif strcmp( C(ii) , 'End' )
-    if ~strcmp( C(ii+1) , 'Site' ), error('"End" keyword must be followed by "Site".'); end
+        
+  elseif strcmp( [C{ii},' ',C{ii+1}] , 'End Site' )
+    % End effector; unnamed terminating joint
     
     nn = nn+1;
     
+    skeleton(nn).name = ' ';
     skeleton(nn).offset = [str2double(C(ii+4)) ; str2double(C(ii+5)) ; str2double(C(ii+6))];
     skeleton(nn).parent = nn-1; % always the direct child
     skeleton(nn).nestdepth = brace_count;
-    skeleton(nn).is_end_effector = true;
-    skeleton(nn).channels = 0;
+    skeleton(nn).Nchannels = 0;
         
   end
   
@@ -80,9 +71,9 @@ end
 
 %% Initial processing and error checking
 
-Nnodes = length(skeleton);
-Nchannels = sum([skeleton.channels]);
-Nchainends = sum([skeleton.is_end_effector]);
+Nnodes = numel(skeleton);
+Nchannels = sum([skeleton.Nchannels]);
+Nchainends = sum([skeleton.Nchannels]==0);
 
 % Calculate number of header lines:
 %  - 5 lines per joint
@@ -106,13 +97,6 @@ if size(rawdata.data,1) ~= Nframes
   warning('LOADBVH:frames_wrong','Error reading BVH file: frames count does not match; continuing anyway.')
 end
 
-%% Define transformations
-
-Rx = @(t) [1 0 0; 0 cosd(t) -sind(t); 0 sind(t) cosd(t)];
-Ry = @(t) [cosd(t) 0 sind(t); 0 1 0; -sind(t) 0 cosd(t)];
-Rz = @(t) [cosd(t) -sind(t) 0; sind(t) cosd(t) 0; 0 0 1];
-TransM = @(R,d) [R, d; 0 0 0 1];
-
 %% Load data into skeleton structure
 %
 % Assume for now that all rotations are in the order ZXY
@@ -121,10 +105,10 @@ channel_count = 0;
 
 for nn = 1:Nnodes
     
-  if skeleton(nn).channels == 6
+  if skeleton(nn).Nchannels == 6
     
-    skeleton(nn).Dxyz = repmat(skeleton(1).offset,[1 Nframes]) + rawdata.data(:,channel_count+[1 2 3])';
-    rot_data = rawdata.data(:,channel_count+[4 5 6])';
+    % assume translational data is always ordered XYZ
+    skeleton(nn).Dxyz = repmat(skeleton(nn).offset,[1 Nframes]) + rawdata.data(:,channel_count+[1 2 3])';
     
     skeleton(nn).rot = nan(3,Nframes);
     for ii = 1:length(skeleton(nn).order)
@@ -141,27 +125,11 @@ for nn = 1:Nnodes
     % Kinematics of the root element:
     skeleton(nn).trans = nan(4,4,Nframes);
     for ff = 1:Nframes
-      
-      rotM = eye(3);
-      for ii = 1:length(skeleton(nn).order)
-        switch skeleton(nn).order(ii)
-          case 'X'
-            rotM = rotM*Rx(skeleton(nn).rot(1,ff));
-          case 'Y'
-            rotM = rotM*Ry(skeleton(nn).rot(2,ff));
-          case 'Z'
-            rotM = rotM*Rz(skeleton(nn).rot(3,ff));
-        end
-      end
-    
-      skeleton(nn).trans(:,:,ff) = TransM(rotM,skeleton(nn).Dxyz(:,ff));
-      
+      skeleton(nn).trans(:,:,ff) = transformation_matrix(skeleton(nn).Dxyz(:,ff) , skeleton(nn).rot(:,ff) , skeleton(nn).order);
     end
     
-  elseif skeleton(nn).channels == 3
-    
-    rot_data = rawdata.data(:,channel_count+[1 2 3])';
-    
+  elseif skeleton(nn).Nchannels == 3
+        
     skeleton(nn).rot = nan(3,Nframes);
     for ii = 1:length(skeleton(nn).order)
       switch skeleton(nn).order(ii)
@@ -177,7 +145,7 @@ for nn = 1:Nnodes
     skeleton(nn).Dxyz  = nan(3,Nframes);
     skeleton(nn).trans = nan(4,4,Nframes);
     
-  elseif skeleton(nn).channels == 0
+  elseif skeleton(nn).Nchannels == 0
     
     skeleton(nn).Dxyz  = nan(3,Nframes);
     
@@ -185,43 +153,66 @@ for nn = 1:Nnodes
     error('Not sure how to handle not (0 or 3 or 6) number of channels.')
   end
   
-  channel_count = channel_count + skeleton(nn).channels;
+  channel_count = channel_count + skeleton(nn).Nchannels;
   
 end
 
 %% Calculate kinematics
 
-for nn = find([skeleton.parent] ~= 0)
+for nn = find([skeleton.parent] ~= 0 & [skeleton.Nchannels] ~= 0)
   
   parent = skeleton(nn).parent;
   
   for ff = 1:Nframes
     
-    rotM = eye(3);
-    for ii = 1:length(skeleton(nn).order)
-      switch skeleton(nn).order(ii)
-        case 'X'
-          rotM = rotM*Rx(skeleton(nn).rot(1,ff));
-        case 'Y'
-          rotM = rotM*Ry(skeleton(nn).rot(2,ff));
-        case 'Z'
-          rotM = rotM*Rz(skeleton(nn).rot(3,ff));
-      end
-    end
-      
-    skeleton(nn).trans(:,:,ff) = skeleton(parent).trans(:,:,ff) * TransM(rotM,skeleton(nn).offset);
+    transM = transformation_matrix( skeleton(nn).offset , skeleton(nn).rot(:,ff) , skeleton(nn).order );
+    skeleton(nn).trans(:,:,ff) = skeleton(parent).trans(:,:,ff) * transM;
     skeleton(nn).Dxyz(:,ff) = skeleton(nn).trans([1 2 3],4,ff);
     
   end
 
 end
 
-
-
-
-
-
+for nn = find([skeleton.Nchannels] == 0)
+  
+  parent = skeleton(nn).parent;
+  
+  for ff = 1:Nframes
+    
+    transM = transformation_matrix( skeleton(nn).offset , [0; 0; 0] , 'ABC' );
+    transM = skeleton(parent).trans(:,:,ff) * transM;
+    skeleton(nn).Dxyz(:,ff) = transM([1 2 3],4);
+    
   end
+
+end
+
+end
+
+
+
+function transM = transformation_matrix(displ,rxyz,order)
+
+cx = cosd(rxyz(1));
+cy = cosd(rxyz(2));
+cz = cosd(rxyz(3));
+sx = sind(rxyz(1));
+sy = sind(rxyz(2));
+sz = sind(rxyz(3));
+
+rotM = eye(3);
+for ii = 1:length(order)
+  switch order(ii)
+    case 'X'
+      rotM = rotM*[1 0 0; 0 cx -sx; 0 sx cx];
+    case 'Y'
+      rotM = rotM*[cy 0 sy; 0 1 0; -sy 0 cy];
+    case 'Z'
+      rotM = rotM*[cz -sz 0; sz cz 0; 0 0 1];
+  end
+end
+
+transM = [rotM, displ; 0 0 0 1];
 
 end
 
