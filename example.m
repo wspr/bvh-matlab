28283de5e2207d34a70ba1e66e1e54001b328dad
Will Robertson <wspr81@gmail.com>
2018-08-09 23:37:59 +0930

    comment out plot_coord line
    this is a function I never got around to releasing. Not really necessary here except for
    for some debugging. I think.
    
    closes #2

--- example.m
+++ example.m
@@ -22,11 +22,11 @@ for ff = 50
   set(h,'color','white')
 
   for nn = 1:length(skeleton)
-    
-    if skeleton(nn).Nchannels ~= 0
-      plot_coord( skeleton(nn).Dxyz(:,ff) , 'length',8,'headlength',3,'index',num2str(nn),'rotate',skeleton(nn).trans(1:3,1:3,ff))
-    end
-    
+
+%    if skeleton(nn).Nchannels ~= 0
+%      plot_coord( skeleton(nn).Dxyz(:,ff) , 'length',8,'headlength',3,'index',num2str(nn),'rotate',skeleton(nn).trans(1:3,1:3,ff))
+%    end
+
     plot3(skeleton(nn).Dxyz(1,ff),skeleton(nn).Dxyz(2,ff),skeleton(nn).Dxyz(3,ff),'.','markersize',20)
 
     parent = skeleton(nn).parent;
@@ -37,11 +37,11 @@ for ff = 50
     end
 
   end
-  
+
   view(-30,30)
   axis equal off
   drawnow
-  
+
   if write_video, writeVideo(vidObj,getframe); end
 
 end
